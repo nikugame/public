4ff4bb4ce97446afb862e55d952607261f2ff0a3
longjianhui <longjianhui@nikugame.com>
2016-11-23 15:44:54 +0800

    修改集群读取方法

--- mq/client.go
+++ mq/client.go
@@ -5,8 +5,6 @@ import (
 
 	"time"
 
-	"strings"
-
 	"github.com/Shopify/sarama"
 	conf "github.com/nikugame/public/config"
 )
@@ -18,7 +16,7 @@ type MsgContent struct {
 }
 
 type Client struct {
-	Addrs       string
+	Addrs       []string
 	Producer    sarama.SyncProducer
 	AsyncEnable bool
 	config      *sarama.Config
@@ -32,7 +30,7 @@ func (s *Client) init() {
 	if len(s.Addrs) == 0 {
 		//logs.Info("没有设置kafka的主机或端口，使用配置文件。")
 		cf, _ := conf.NewConfig("ini", "conf/settings.conf")
-		s.Addrs = cf.String("MqServer::addrs")
+		s.Addrs = cf.Strings("MqServer::addrs")
 		//fmt.Println(s.Addrs)
 		// s.connString = cf.String("MqServer::Host") + ":" + cf.String("MqServer::Port")
 		//logs.Info("连接字符串为：%s", s.Addrs)
@@ -43,7 +41,7 @@ func (s *Client) init() {
 func (s *Client) Send() (int32, int64, error) {
 	s.init()
 	var err error
-	s.Producer, err = sarama.NewSyncProducer(strings.Split(s.Addrs, ","), s.config)
+	s.Producer, err = sarama.NewSyncProducer(s.Addrs, s.config)
 	if err != nil {
 		fmt.Println(err)
 	}
